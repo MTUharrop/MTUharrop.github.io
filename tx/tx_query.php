9df81a7fdb26e2a247143450d09ccd06e7aa70a8
MTUharrop <119802902+MTUharrop@users.noreply.github.com>
2022-12-12 10:23:15 +0000

    Create tx_query.php

---
+++ tx/tx_query.php
@@ -0,0 +1,8 @@
+<html>
+  <body>
+    <h3>Transaction Details:</h3><br>
+    Transaction ID: <?php echo $_GET["txID"]; ?><br>
+    Transaction Date: <?php echo $_GET["txDate"]; ?><br>
+    Payment Method: <?php echo $_GET["txPayMethod"]; ?><br>
+  </body>
+</html>
